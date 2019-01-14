9b4cff36b40f142a96fb84e114b5118b4152deec
schmartmann <stefanhartmann@gmail.com>
2019-01-14 11:41:49 -0500

    create new app strucutre

---
+++ src/app/app.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './app.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app-container">
+        <p>Welcome to Lunch Drunk!</p>
+      </div>
+    );
+  }
+}
+
+export default App;
